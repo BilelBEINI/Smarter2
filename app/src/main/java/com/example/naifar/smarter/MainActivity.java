28c72b80adeb8a489f1529ffa6c4dbe34b6b2153
zamama <mourad.naifar@gmail.com>
2017-04-24 13:44:56 +0200

    Second Commit

--- app/src/main/java/com/example/naifar/smarter/MainActivity.java
+++ app/src/main/java/com/example/naifar/smarter/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplication(),"Long",Toast.LENGTH_LONG).show();
-        // second 
+        // second
+        // gdhjd
     }
 }
